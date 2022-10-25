18d125ea048abc70b703e9dbca2e3bf7aab95095
o.pyhov <o.pyhov@prosoftsystems.ru>
2022-10-25 12:18:52 +0500

    make cursir blink

--- lua/settings.lua
+++ lua/settings.lua
@@ -2,18 +2,19 @@ local cmd = vim.cmd -- execute Vim commands
 local opt = vim.opt -- global/buffer/windows-scoped options
 
 -- General
-opt.colorcolumn = '100' -- set ruler position
-opt.number = true       -- lines number on
-opt.cursorline = true   -- highlight the screen line of the cursor
-opt.wrap = false        -- do not wrap long lines
-opt.undofile = true    	-- rolling back file (?)
-opt.splitright = true  	-- open vertical split always at right
-opt.splitbelow = true  	-- open horizontal split always below
-opt.expandtab = true	-- use spaces instead of tabs
-opt.shiftwidth = 4	    -- shift 4 spaces when tab
-opt.softtabstop = 4	    -- shift 4 spaces when tab
-opt.tabstop = 4		    -- 1 tab == 4 spaces
-opt.smartindent = true  -- autoindent new lines
+opt.colorcolumn = '100'         -- set ruler position
+opt.number = true               -- lines number on
+opt.cursorline = true           -- highlight the screen line of the cursor
+opt.wrap = false                -- do not wrap long lines
+opt.undofile = true    	        -- rolling back file (?)
+opt.splitright = true  	        -- open vertical split always at right
+opt.splitbelow = true  	        -- open horizontal split always below
+opt.expandtab = true	        -- use spaces instead of tabs
+opt.shiftwidth = 4	            -- shift 4 spaces when tab
+opt.softtabstop = 4	            -- shift 4 spaces when tab
+opt.tabstop = 4		            -- 1 tab == 4 spaces
+opt.smartindent = true          -- autoindent new lines
+opt.guicursor = "a:blinkon500"  -- make cursor blink
  -- do not autocomment new lines
 cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
 
